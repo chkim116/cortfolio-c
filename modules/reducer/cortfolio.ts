507a26f2650e0071f1b58550fcac97f81ddff738
chkim116 <dudnqnfqlc@naver.com>
2021-01-31 09:52:18 +0900

    Add : cortfolio 리듀서 init

---
+++ modules/reducer/cortfolio.ts
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cortfolio = createSlice({
+    name: "cortfolio",
+    initialState: {},
+    reducers: {},
+});
+
+export const {} = cortfolio.actions;
+
+export default cortfolio.reducer;
